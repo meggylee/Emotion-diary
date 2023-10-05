e75c46d318addd13707e4fc90a5d585b38ed118a
[SW5] 이혜진 <meggylee792@gmail.com>
2023-10-06 01:07:51 +0900

    기본 컴포넌트 작업

---
+++ src/pages/Edit.js
@@ -0,0 +1,32 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+const Edit = () => {
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get('id');
+    console.log(id);
+    return (
+        <div>
+            <h1>Edit</h1>
+            <p>이곳은 일기 수정 페이지 입니다.</p>
+            <button onClick={() => setSearchParams({ who: 'winterlood' })}>QS바꾸기</button>
+            <button
+                onClick={() => {
+                    navigate('/home');
+                }}
+            >
+                HOME으로 가기
+            </button>
+            <button
+                onClick={() => {
+                    navigate(-1);
+                }}
+            >
+                뒤로가기
+            </button>
+        </div>
+    );
+};
+
+export default Edit;
