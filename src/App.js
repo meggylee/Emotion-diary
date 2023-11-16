78f9417a1e96196d6cd845b83efcf42ee24acf35
[SW5] 이혜진 <meggylee792@gmail.com>
2023-11-17 01:29:25 +0900

    다이어리 수정하기 기능

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const reducer = (state, action) => {
             break;
         }
         case 'EDIT': {
-            newState = state.map((it) => (action.data.id ? { ...action.data } : it));
+            newState = state.map((it) => (it.id === action.data.id ? { ...action.data } : it));
             break;
         }
         default:
@@ -117,7 +117,7 @@ function App() {
                         <Routes>
                             <Route path="/" element={<Home />} />
                             <Route path="/new" element={<New />} />
-                            <Route path="/edit" element={<Edit />} />
+                            <Route path="/edit/:id" element={<Edit />} />
                             <Route path="/diary/:id" element={<Diary />} />
                         </Routes>
                     </div>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,32 +1,26 @@
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { DiaryStateContext } from '../App';
+import DiaryEditor from '../components/DiaryEditor';
 
 const Edit = () => {
+    const [originData, setOriginData] = useState();
     const navigate = useNavigate();
-    const [searchParams, setSearchParams] = useSearchParams();
+    const { id } = useParams();
+    const diaryList = useContext(DiaryStateContext);
+    console.log()
 
-    const id = searchParams.get('id');
-    console.log(id);
-    return (
-        <div>
-            <h1>Edit</h1>
-            <p>이곳은 일기 수정 페이지 입니다.</p>
-            <button onClick={() => setSearchParams({ who: 'winterlood' })}>QS바꾸기</button>
-            <button
-                onClick={() => {
-                    navigate('/home');
-                }}
-            >
-                HOME으로 가기
-            </button>
-            <button
-                onClick={() => {
-                    navigate(-1);
-                }}
-            >
-                뒤로가기
-            </button>
-        </div>
-    );
+    useEffect(() => {
+        if (diaryList.length >= 1) {
+            const targetDiary = diaryList.find((it) => parseInt(it.id) === parseInt(id));
+            if (targetDiary) {
+                setOriginData(targetDiary);
+            } else {
+                navigate('/', { replace: true });
+            }
+        }
+    }, [id, diaryList]);
+    return <div>{originData && <DiaryEditor isEdit={true} originData={originData} />}</div>;
 };
 
 export default Edit;
